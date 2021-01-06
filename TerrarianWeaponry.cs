bdf3a636052bb66829d9475703f2532053743288
Chik3r <ikervaraalarcon@gmail.com>
2021-01-06 16:02:53 -0300

    Small changes

--- TerrarianWeaponry.cs
+++ TerrarianWeaponry.cs
@@ -1,25 +1,9 @@
-using System.Linq;
-using Terraria;
 using Terraria.ModLoader;
-using TerrarianWeaponry.Items;
 
 namespace TerrarianWeaponry
 {
 	public class TerrarianWeaponry : Mod
 	{
-		public override void Load()
-		{
-			//BaseItem item = new BaseItem
-			//{
-			//	info =
-			//	{
-			//		damage = 200,
-			//		knockBack = 2f
-			//	}
-			//};
-			//AddItem("StrongSword", item);
-			//var fields = typeof(Item).GetFields();
-			//Logger.Debug(string.Join("\n", fields.Select(x => $"{x.Name} - {x.FieldType}")));
-		}
+		
 	}
 }
