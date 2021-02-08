dc47a45076e6094be330199ca209cba92e2c0688
Chik3r <ikervaraalarcon@gmail.com>
2021-02-08 16:05:22 -0300

    Remove legacy loading

--- JsonData/ToolData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TerrarianWeaponry.JsonData
-{
-	public class ToolData
-	{
-		public string ToolName { get; set; }
-		public string[] Parts { get; set; }
-	}
-}
--- TerrarianWeaponry.cs
+++ TerrarianWeaponry.cs
@@ -1,25 +1,15 @@
-using System.Text;
 using Terraria.ModLoader;
-using TerrarianWeaponry.JsonData;
+using TerrarianWeaponry.DataLoading;
 
 namespace TerrarianWeaponry
 {
 	public class TerrarianWeaponry : Mod
 	{
 		public static TerrarianWeaponry Instance => ModContent.GetInstance<TerrarianWeaponry>();
-		
-		internal DataLoader loader;
 
 		public override void Load()
 		{
-			byte[] jsonBytes = GetFileBytes("JsonData/data.json");
-			string jsonString = Encoding.UTF8.GetString(jsonBytes);
-			var mainData = MainData.FromJson(jsonString);
-
-			loader = new DataLoader(mainData);
-			loader.LoadData();
-
-			DataLoading.DataLoader.LoadData();
+			DataLoader.LoadData();
 		}
 	}
 }
