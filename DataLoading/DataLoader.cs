97d73de345047a55220380fef33ba0b485142a6a
Chik3r <ikervaraalarcon@gmail.com>
2021-02-08 16:16:31 -0300

    A little bit of cleaning up

--- DataLoading/DataLoader.cs
+++ DataLoading/DataLoader.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using TerrarianWeaponry.DataLoading.Data;
-using TerrarianWeaponry.DataLoading.Data.Parts;
 
 namespace TerrarianWeaponry.DataLoading
 {
@@ -13,38 +10,32 @@ namespace TerrarianWeaponry.DataLoading
 	{
 		public static void LoadData()
 		{
-			// BaseTool
-			//  -> Part Foo
-			//		-> Material Alpha
-			//		-> Material Gamma
-			//  -> Part Bar
-			//		-> Material Beta
-			//		-> Material Gamma
-
 			foreach (BaseTool baseTool in GetTools())
 			{
-				//float maxModifier = LoadParts(baseTool)
-				//	.Max(p => 
-				//		LoadMaterials(p).Max(m => m.Modifier)) ?? 1f;
 				var combos = GetMaterialCombinations(baseTool);
 				foreach (var materialCombo in combos)
 				{
+					// Make a new ItemInfo and get the max modifier of all materials
 					var info = new ItemInfo();
 					float maxModifier = materialCombo.Max(m => m.Modifier) ?? 1f;
+
+					// Make the tool name be like this: "ToolName: Material1-Material2-...-MaterialN"
 					string toolName = $"{baseTool.ToolName}: {string.Join("-", materialCombo.Select(x => x.MaterialName))}";
 
+					// Loop over the materials and modify the info
 					foreach (BaseMaterial material in materialCombo)
 						material.ModifyStats(ref info, maxModifier);
 
+					// Calculate the item's texture from the materials
 					var textures = materialCombo.Select(m => TerrarianWeaponry.Instance.GetTexture(m.Texture)).ToArray();
 					Point originPoint = materialCombo.First().OriginPoint;
 					Texture2D finalTexture = textures.First();
 
 					for (int i = 1; i < textures.Length; i++)
-					{
-						finalTexture = Utilities.MixTexture2D(finalTexture, originPoint, textures[i], materialCombo[i].OriginPoint);
-					}
+						finalTexture = Utilities.MixTexture2D(finalTexture, originPoint, 
+							textures[i], materialCombo[i].OriginPoint);
 					
+					// Register the item
 					RegisterItem(baseTool, toolName, info, finalTexture);
 				}
 			}
@@ -52,22 +43,24 @@ namespace TerrarianWeaponry.DataLoading
 
 		private static void RegisterItem(BaseTool tool, string itemName, ItemInfo info, Texture2D texture)
 		{
+			// Get the type and create an instance of it
 			var toolType = tool.GetType();
 			var finalItem = (BaseTool) Activator.CreateInstance(toolType, info, texture);
 
+			// Then register it
 			TerrarianWeaponry.Instance.AddItem(itemName, finalItem);
 		}
 
 		private static IEnumerable<BaseTool> GetTools()
 		{
+			// Get all types extending BaseTool
 			var baseType = typeof(BaseTool);
 			var childTypes = TerrarianWeaponry.Instance.Code.GetTypes()
 				.Where(t => baseType.IsAssignableFrom(t) && !t.IsAbstract);
 
+			// And return an IEnumerable from an instance of the types
 			foreach (Type childType in childTypes)
-			{
 				yield return (BaseTool) Activator.CreateInstance(childType, new object[] {null, null});
-			}
 		}
 
 		private static List<List<BaseMaterial>> GetMaterialCombinations(BaseTool tool)
