3b7a50b44cfc317ee968e7817b1d17987bae9306
Chik3r <ikervaraalarcon@gmail.com>
2021-02-12 15:06:01 -0300

    Add a UI debug command

---
+++ DebugCommands.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ModLoader;
+using TerrarianWeaponry.UI;
+
+namespace TerrarianWeaponry
+{
+#if DEBUG
+	class DebugUICommand : ModCommand
+	{
+		public override void Action(CommandCaller caller, string input, string[] args)
+		{
+			TerrarianWeaponry.Instance.UserInterface.SetState(null);
+			var _toolForgeUI = new MaterialInfoState();
+			_toolForgeUI.Activate();
+			TerrarianWeaponry.Instance.UserInterface.SetState(_toolForgeUI);
+		}
+
+		public override string Command => "ui";
+		public override CommandType Type => CommandType.Chat;
+	}
+#endif
+}
