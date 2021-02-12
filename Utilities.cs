300c5d019b09c841e0e1d4401b69254f903006ac
Chik3r <ikervaraalarcon@gmail.com>
2021-02-12 15:06:28 -0300

    Add a generic method to get a list of objects extending T

--- Utilities.cs
+++ Utilities.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
@@ -153,5 +155,17 @@ namespace TerrarianWeaponry
 		}
 
 		#endregion
+
+		internal static IEnumerable<T> GetTypesExtendingT<T>(params object[] constructorArgs)
+		{
+			// Get all types extending T
+			var baseType = typeof(T);
+			var childTypes = TerrarianWeaponry.Instance.Code.GetTypes()
+				.Where(t => baseType.IsAssignableFrom(t) && !t.IsAbstract);
+
+			// And return an IEnumerable from an instance of the types
+			foreach (Type childType in childTypes)
+				yield return (T)Activator.CreateInstance(childType, constructorArgs);
+		}
 	}
 }
