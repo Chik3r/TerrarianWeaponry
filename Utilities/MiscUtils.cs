a39308d737bee85704ae32899fcfc29d7fe9c494
Chik3r <ikervaraalarcon@gmail.com>
2021-02-18 19:50:11 -0300

    Add util to consume item

--- Utilities/MiscUtils.cs
+++ Utilities/MiscUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Terraria;
 
 namespace TerrarianWeaponry.Utilities
 {
@@ -16,6 +17,19 @@ namespace TerrarianWeaponry.Utilities
 			// And return an IEnumerable from an instance of the types
 			foreach (Type childType in childTypes)
 				yield return (T)Activator.CreateInstance(childType, constructorArgs);
+		}
+
+		public static bool SafeConsume(this Item item, int amount = 1)
+		{
+			if (item.stack < amount)
+				return false;
+
+			item.stack -= amount;
+
+			if (item.stack == 0)
+				item.TurnToAir();
+
+			return true;
 		}
 	}
 }
